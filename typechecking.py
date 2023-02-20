385fa6ee77fae0431f0dddf19f971d21a1eda501
DhairyaShah981 <76472249+DhairyaShah981@users.noreply.github.com>
2023-02-20 14:15:11 +0530

    resolved errors in typechecker

--- typechecking.py
+++ typechecking.py
@@ -4,12 +4,27 @@ from fractions import Fraction
 from typing import Union, Optional, NewType
 import pytest
 
-# Literals
+
+@dataclass
+class NumType:
+    pass
 
 
 @dataclass
+class BoolType:
+    pass
+
+
+@dataclass
+class StringType:
+    pass
+
+
+# Literals
+@dataclass
 class numeric_literal:
     value: Fraction
+    type: NumType = NumType()
 
     def __init__(self, numerator, denominator=1):
         self.value = Fraction(numerator, denominator)
@@ -18,11 +33,13 @@ class numeric_literal:
 @dataclass
 class bool_literal:
     value: bool
+    type: BoolType = BoolType()
 
 
 @dataclass
 class string_literal:
     value: str
+    type: StringType = StringType()
 
 
 # Binary Operations(Arithmetic and Boolean)
@@ -31,12 +48,14 @@ class binary_operation:
     operator: str
     left: "AST"
     right: "AST"
+    type: Optional[Union[NumType, BoolType]] = None
 
 
 @dataclass
 class unary_operation:
     operator: str
     operand: "AST"
+    type: Optional[Union[NumType, BoolType]] = None
 
 # String operation (Can take variable number of strings depending on the operation)
 
@@ -45,6 +64,7 @@ class unary_operation:
 class string_concat:
     operator: str
     operands: List["AST"]
+    type: StringType = StringType()
 
 
 @dataclass
@@ -54,20 +74,26 @@ class string_slice:
     start: "AST"
     stop: "AST"
     hop: "AST" = numeric_literal(1)
+    type: StringType = StringType()
 
 
 # Let Expressions
 @dataclass
 class let_var:
     name: str
-
+    type: Optional[Union[NumType, BoolType, StringType]] = None
 
 # variables
+
+
 @dataclass
 class let:
     variable: let_var
     e1: "AST"
     e2: "AST"
+    type: Optional[Union[NumType, BoolType, StringType]] = None
+
+# left to typecheck
 
 
 @dataclass
@@ -106,6 +132,7 @@ class if_statement:
 class while_loop:
     condition: "AST"
     body: "AST"
+    type: Optional[Union[NumType, BoolType, StringType]] = None
 
 # For loop
 
@@ -116,6 +143,7 @@ class for_loop:
     condition: "AST"
     updation: "AST"
     body: "AST"
+    type: Optional[Union[NumType, BoolType, StringType]] = None
 
 
 @dataclass
@@ -172,93 +200,87 @@ def typecheck(program: AST, env=None) -> TypedAST:
             return t
         case bool_literal() as t:  # already typed.
             return t
-        case binary_operation(op, left, right) if op in ["+", "-", "*", "/", "%"]:
-            tleft = typecheck(left)
-            tright = typecheck(right)
-            if tleft.type != numeric_literal() or tright.type != numeric_literal():
-                raise TypeError()
-            if op in ["+", "-", "*", "/"]:
-                return binary_operation(op, left, right, numeric_literal())
-            else:
-                return binary_operation(op, left, right, numeric_literal(1))
-        case binary_operation(op, left, right) if op in ["<", ">", "<=", ">="]:
-            tleft = typecheck(left)
-            tright = typecheck(right)
-            if tleft.type != numeric_literal() or tright.type != numeric_literal():
-                raise TypeError()
-            return binary_operation(op, left, right, bool_literal())
-        case binary_operation("=", left, right):
-            tleft = typecheck(left)
-            tright = typecheck(right)
-            if tleft.type != tright.type:
-                raise TypeError()
-            return binary_operation("=", left, right, bool_literal())
-        # case bool_operation(op, left, right) if op in ["and", "or"]:
-        #     tleft = typecheck(left)
-        #     tright = typecheck(right)
-        #     if tleft.type != bool_literal() or tright.type != bool_literal():
-        #         raise TypeError()
-        #     return binary_operation(op, left, right, bool_literal())
-        case unary_operation(op, operand) if op == "not":
-            top = typecheck(operand)
-            if top.type != bool_literal():
-                raise TypeError()
-            return unary_operation(op, operand, bool_literal())
         case string_literal() as t:  # already typed.
             return t
-        case string_concat(op, operands):
-            typed_operands = []
-            for operand in operands:
-                typed_operands.append(typecheck(operand))
-            return string_concat(op, typed_operands, string_literal())
-        case string_slice(op, string, start, stop, hop):
-            typed_string = typecheck(string)
-            typed_start = typecheck(start)
-            typed_stop = typecheck(stop)
-            typed_hop = typecheck(hop)
-            if typed_string.type != string_literal() or typed_start.type != numeric_literal() \
-                    or typed_stop.type != numeric_literal() or typed_hop.type != numeric_literal():
+
+        case unary_operation(op, operand) if op == "not":
+            top = typecheck(operand)
+            if top.type != BoolType():
                 raise TypeError()
-            return string_slice(op, typed_string, typed_start, typed_stop, typed_hop, string_literal())
-        case let(variable, e1, e2):
-            t1 = typecheck(e1)
-            env.add_to_scope(variable.name, t1)
-            t2 = typecheck(e2)
-            env.end_scope()
-            return t2
-        case mut_var(name):
-            return mut_var(name)
-        case declare(variable, value):
-            typed_value = typecheck(value)
-            env.add_to_scope(variable.name, typed_value)
-            return typed_value
-        case get(variable):
-            return env.get_from_scope(variable.name)
-        case set(variable, value):
-            typed_value = typecheck(value)
-            env.update_scope(variable.name, typed_value)
-            return typed_value
+            return unary_operation(op, operand, BoolType())
+        case binary_operation(op, left, right):
+            tl = typecheck(left)
+            tr = typecheck(right)
+            if op in ["+", "-", "*", "/"]:
+                if tl.type != NumType() or tr.type != NumType():
+                    raise TypeError()
+                return binary_operation(op, tl, tr, NumType())
+            elif op in ["==", "!=", "<", ">", "<=", ">="]:
+                if tl.type != tr.type:
+                    raise TypeError()
+                return binary_operation(op, tl, tr, BoolType())
+            elif op in ["and", "or"]:
+                if tl.type != BoolType() or tr.type != BoolType():
+                    raise TypeError()
+                return binary_operation(op, tl, tr, BoolType())
+            else:
+                raise TypeError()
+
         case if_statement(c, t, f):
             tc = typecheck(c)
-            if tc.type != bool_literal():
+            if tc.type != BoolType():
                 raise TypeError()
             tt = typecheck(t)
             tf = typecheck(f)
             if tt.type != tf.type:
                 raise TypeError()
             return if_statement(tc, tt, tf, tt.type)
-
+        
+# case string_concat(op, operands):
+        #     typed_operands = []
+        #     for operand in operands:
+        #         typed_operands.append(typecheck(operand))
+        #     return string_concat(op, typed_operands, string_literal())
+        # case string_slice(op, string, start, stop, hop):
+        #     typed_string = typecheck(string)
+        #     typed_start = typecheck(start)
+        #     typed_stop = typecheck(stop)
+        #     typed_hop = typecheck(hop)
+        #     if typed_string.type != string_literal() or typed_start.type != numeric_literal() \
+        #             or typed_stop.type != numeric_literal() or typed_hop.type != numeric_literal():
+        #         raise TypeError()
+        #     return string_slice(op, typed_string, typed_start, typed_stop, typed_hop, string_literal())
+        # case let(variable, e1, e2):
+        #     t1 = typecheck(e1)
+        #     env.add_to_scope(variable.name, t1)
+        #     t2 = typecheck(e2)
+        #     env.end_scope()
+        #     return t2
+        # case mut_var(name):
+        #     return mut_var(name)
+        # case declare(variable, value):
+        #     typed_value = typecheck(value)
+        #     env.add_to_scope(variable.name, typed_value)
+        #     return typed_value
+        # case get(variable):
+        #     return env.get_from_scope(variable.name)
+        # case set(variable, value):
+        #     typed_value = typecheck(value)
+        #     env.update_scope(variable.name, typed_value)
+        #     return typed_value
     raise TypeError()
 
 
 def test_typecheck():
-
-    te = typecheck(binary_operation(
-        "+", numeric_literal(2), numeric_literal(3)))
-    assert te.type == numeric_literal()
-    te = typecheck(binary_operation(
-        "<", numeric_literal(2), numeric_literal(3)))
-    assert te.type == bool_literal()
+    e1 = numeric_literal(2)
+    e2 = numeric_literal(3)
+    te = typecheck(binary_operation("+", e1, e2))
+    te = typecheck(binary_operation("<", e1, e2))
+    assert te.type == NumType()
+    assert te.type == BoolType()
     with pytest.raises(TypeError):
-        typecheck(binary_operation("+", binary_operation("*", numeric_literal(2), numeric_literal(3)),
-                  binary_operation("<", numeric_literal(2), numeric_literal(3))))
+        typecheck(binary_operation("+", binary_operation("*", numeric_literal(2),
+                                                         numeric_literal(3)), binary_operation("<", numeric_literal(2), numeric_literal(3))))
+
+
+test_typecheck()
