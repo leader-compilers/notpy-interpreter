bb18c7dc511365851dbede8534f826478a31d1a9
Medhansh <medhansh.singh@iitgn.ac.in>
2023-04-03 12:06:46 +0530

    merging

--- bytecode_test.py
+++ bytecode_test.py
@@ -134,33 +134,6 @@ def test3_unaryOps():
     v.load(compile(e2))
     assert(v.execute() == 1)
 
-from bytecode import *
-
-def test1_binOps():
-    e1 = binary_operation("+", numeric_literal(1), numeric_literal(2))
-    # assert(codegen(e1).insns == [I.PUSH(1), I.PUSH(2), I.ADD(), I.HALT()])
-    v = VM()
-    v.load(compile(e1))
-    assert(v.execute() == 3)
-
-def test2_stringOps():
-    e1 = []
-    for i in range(4):
-        e1.append(string_literal(str(i)))
-    e2 = string_concat(e1)
-    v = VM()
-    v.load(compile(e2))
-    assert(v.execute() == "0123")
-
-    words = ["This", "Is", "A", "Test", "For", " ", "Concatenation"]
-    e3 = []
-    for i in words:
-        e3.append(string_literal(i))
-    e4 = string_concat(e3)
-    v = VM()
-    v.load(compile(e4))
-    assert(v.execute() == "ThisIsATestFor Concatenation")
-
 def test4_print():
     e1 = numeric_literal(1)
     e2 = numeric_literal(2)
@@ -192,6 +165,4 @@ def test4_print():
 test1_binOps()
 test2_stringOps()
 test3_unaryOps()
-test1_binOps()
-test2_stringOps()
 test4_print()
