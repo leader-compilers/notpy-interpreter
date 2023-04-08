bee927e2a5fbdd471c71849f4c1cfa9a7a443f6a
DhairyaShah981 <76472249+DhairyaShah981@users.noreply.github.com>
2023-04-08 12:46:10 +0530

    added typechecker and resolver to the pipeline

--- loader.py
+++ loader.py
@@ -1,18 +1,21 @@
 import lexer as l
 import Parser as p
 import eval as e
-
+import typechecking as t
+import resolver as r
 
 
 def main(filename):
     with open(filename) as f:
         code = f.read()
-    # code = '{' + code + '}'
+    code = '{' + code + '}'
     stream = l.Stream.streamFromString(code)
     tokens = l.lexer.lexerFromStream(stream)
     parse = p.Parser.call_parser(tokens)
     ast = p.Parser.parse_expr(parse)
-    # print(ast)
-    output = e.eval_ast(ast)
+    resolvedast = r.resolve(ast)
+    typedast = t.typecheck(resolvedast)
+    output = e.eval_ast(typedast)
 
-main("euler4.txt")
+
+main("euler1.txt")
